d4ee92fc6f8131ae2b009d240964f6ff6ddbd038
KristofferKvale <e.g.kristoffer@gmail.com>
2023-10-31 22:12:04 +0100

    fix left margin

--- src/app/components/Background.tsx
+++ src/app/components/Background.tsx
@@ -7,7 +7,7 @@ export default function Background({ src, alt }: { src: string; alt: string }) {
       alt={alt}
       width={1000}
       height={1000}
-      className='z-0 fixed top-0 place-self-center min-w-full min-h-full aspect-square'
+      className='z-0 fixed top-0 left-0 place-self-center min-w-full min-h-full aspect-square'
     />
   )
 }
